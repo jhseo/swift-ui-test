caa81aed04e78d1398ee159864ae39b1b58a0ae8
jhSeo <jh.seo@battleent.com>
2020-01-30 18:35:16 +0900

    Add a WalletModel

---
+++ swift-ui-test/Model/WalletModel.swift
@@ -0,0 +1,15 @@
+//
+//  WalletModel.swift
+//  swift-ui-test
+//
+//  Created by jhseo on 2020/01/30.
+//  Copyright © 2020 jhseo. All rights reserved.
+//
+
+import Foundation
+
+struct WalletModel {
+    let userId: String
+    let publicAddress: String
+    let tokenAmount: Int64
+}
